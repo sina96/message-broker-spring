7d912e1aba179e100e2a1c4d6222209143b8bc67
sinabastani <s.bastani96@gmail.com>
2025-05-29 18:24:46 +0200

    Initial commit

---
+++ src/test/java/com/sinacodes/message_broker/MessageBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinacodes.message_broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
